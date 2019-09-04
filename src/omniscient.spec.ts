2610ddd056b5e9c8dc7f373b6ad7fa431a2a0ffb
terodox <terodox@gmail.com>
2019-09-04 19:42:30 -0400

    Can now construct and get state

--- src/omniscient.spec.ts
+++ src/omniscient.spec.ts
@@ -4,4 +4,22 @@ test('should exist', () => {
     expect(Omniscient).toEqual(jasmine.any(Function));
 });
 
-test('should ', () => {});
+test('should throw if starting state is not an object', () => {
+    expect(() => new Omniscient<any>(1234)).toThrowError('starting');
+    expect(() => new Omniscient<any>('abcd')).toThrowError('starting');
+    expect(() => new Omniscient<any>(() => {})).toThrowError('starting');
+});
+
+test('should not throw if starting state is an object', () => {
+    expect(() => new Omniscient<any>({})).not.toThrow();;
+});
+
+describe('getState', () => {
+    test('should return the full state if no specifier is passed', () => {
+        const expectedState = { the: 'most expected state' };
+
+        const omniscient = new Omniscient<any>(expectedState);
+
+        expect(omniscient.getState()).toBe(expectedState);
+    });
+});
--- src/omniscient.ts
+++ src/omniscient.ts
@@ -1,3 +1,14 @@
-export class Omniscient {
-    
+export class Omniscient<T> {
+    private _internalState: T;
+
+    constructor(startingState: T) {
+        if(!startingState || typeof startingState !== 'object') {
+            throw new TypeError(`The starting state must be an object. Provided value: ${startingState}`);
+        }
+        this._internalState = startingState;
+    }
+
+    getState() {
+        return this._internalState;
+    }
 }
